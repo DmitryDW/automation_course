f3338a7d5e1bf669b295fc0b94cb4274a5f61d66
DmitryDW <vladimirov_dmitry33@mail.ru>
2023-05-01 23:32:00 +0300

    modified test file

--- test_stepik/test_product_page.py
+++ test_stepik/test_product_page.py
@@ -1,10 +1,26 @@
 import time
-
+import pytest
+from selenium import webdriver
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = ' http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=newYear2019'
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                   pytest.param("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7", marks=pytest.mark.xfail),
+
+                                  #сделали исключение для упавшего теста чтобы он помечался xfail
+
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+
+
+def test_guest_can_add_product_to_basket(browser, link):
+    link = f"{link}"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_basket_page()
@@ -12,6 +28,8 @@ def test_guest_can_add_product_to_basket(browser):
     time.sleep(2)
     page.add_product_to_basket()
     page.name_and_price_from_basket()
+
+
 
 
 
